f2359ef1ac20d6dc1b60004f51e098d3f8a0a647
Ilpo Kettunen <ikettu@gmail.com>
2016-09-18 21:19:35 +0300

    Cleanup

--- JukolaApp/app/store/MenuStore.js
+++ JukolaApp/app/store/MenuStore.js
@@ -1,24 +1,9 @@
 Ext.define('JukolaApp.store.MenuStore', {
     extend: 'Ext.data.TreeStore',
-//    requires: ['JukolaApp.model.MenuModel', 'JukolaApp.store.CleanProxy'],
 
     alias: 'store.menustore',
-
-//    model: 'JukolaApp.model.MenuModel',
     
     root: {
         expanded: true
-    },
-/*    
-    autload:false,
-    
-    proxy: {
-        type:'clean',
-        noCache:false,
-        url:'resources/menu_default.json',
-        reader: {
-            type:'json'
-        }
     }
-    */
 });
