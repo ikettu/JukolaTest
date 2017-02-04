e9ed462e8b58dda0fc7d5c3a3b6118073679802b
Ilpo Kettunen <ikettu@gmail.com>
2017-02-04 15:06:47 +0200

    travis test3

--- travis_s3_deploy.sh
+++ travis_s3_deploy.sh
@@ -1,5 +1,6 @@
 deployprod() {
   sudo apt-get install s3cmd
+  touch ~/.s3cfg
   export AWS_ACCESS_KEY_ID=$JUKOLA_AWS_KEY
   export AWS_SECRET_ACCESS_KEY=$JUKOLA_AWS_SECRET   
   s3cmd sync build/production/JukolaApp s3://jukolatest --acl-public --delete-removed
