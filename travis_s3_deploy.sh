675fbe0b90be8864f98b6cb60f797606f9880246
Ilpo Kettunen <ikettu@gmail.com>
2017-02-04 16:13:04 +0200

    deploy script cleaned.
    Syncinc should now work corretly with md5 checksums

--- travis_s3_deploy.sh
+++ travis_s3_deploy.sh
@@ -7,17 +7,15 @@ deployprod() {
   touch ~/.s3cfg
   export AWS_ACCESS_KEY_ID=$JUKOLA_AWS_KEY
   export AWS_SECRET_ACCESS_KEY=$JUKOLA_AWS_SECRET
-  echo "deploying with $AWS_ACCESS_KEY_ID"
-  s3cmd -d -v --region=eu-west-1 sync build/production/JukolaApp/ s3://jukolatest/ --acl-public --delete-removed
+
+  s3cmd -v --region=eu-west-1 sync build/production/JukolaApp/ s3://jukolatest/ --acl-public --delete-removed
 }
 
-deployprod
-
-#if [[ $TRAVIS_PULL_REQUEST != 'false' ]]; then
-#  echo "This is a pull request. No deployment will be done."
-#elif [[ $TRAVIS_BRANCH == 'master' ]]; then
-#  echo "Going to deploy on master..."
-#  deployprod
-#else
-#  echo "Nothing to do..."
-#fi
+if [[ $TRAVIS_PULL_REQUEST != 'false' ]]; then
+  echo "This is a pull request. No deployment will be done."
+elif [[ $TRAVIS_BRANCH == 'master' ]]; then
+  echo "Going to deploy on master..."
+  deployprod
+else
+  echo "Nothing to do..."
+fi
