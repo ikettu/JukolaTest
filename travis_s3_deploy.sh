af6166d4839048819778fbd1570a513b64ff9137
Ilpo Kettunen <ikettu@gmail.com>
2017-02-04 14:47:04 +0200

    Just testing more intelligent travis deploy

---
+++ travis_s3_deploy.sh
@@ -0,0 +1,17 @@
+deployprod() {
+  sudo apt-get install -y s3cmd
+  export AWS_ACCESS_KEY_ID=$JUKOLA_AWS_KEY
+  export AWS_SECRET_ACCESS_KEY=$JUKOLA_AWS_SECRET   
+  s3cmd sync build/production/JukolaApp s3://jukolatest --acl-public --delete-removed
+}
+
+deployprod
+
+#if [[ $TRAVIS_PULL_REQUEST != 'false' ]]; then
+#  echo "This is a pull request. No deployment will be done."
+#elif [[ $TRAVIS_BRANCH == 'master' ]]; then
+#  echo "Going to deploy on master..."
+#  deployprod
+#else
+#  echo "Nothing to do..."
+#fi
